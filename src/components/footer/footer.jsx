658049f57cd2838cbfcb86f5b6e7294c7a285351
zednegr <negrzed@gmail.com>
2023-08-30 12:26:47 +0500

    i

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,17 @@
+// Import SASS
+import "./footer.scss"
+
+function Footer() {
+    
+    return (
+        <section className="footer-section">
+            <div className="container">
+                <div className="footer-wrapper">
+
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Footer
---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,17 @@
+// Import SASS
+import "./navbar.scss"
+
+function Navbar() {
+    
+    return (
+        <section className="navbar-secrtion">
+            <div className="container">
+                <div className="navbar-wrapper">
+
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Navbar
